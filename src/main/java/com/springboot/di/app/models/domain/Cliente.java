af3c813df5b85a8e3044465228079e398080b368
Luis B <luis.beroiza@gmail.com>
2023-05-19 16:35:57 +0200

    Agrega Scope al componente Spring
    Existen basicamente 4 scopes:
    
    @Componente
      Sin nada es @Stateless de JEE,
      solo una instancia para la aplicacion
      no tiene estado (Mas grande)
    
    @Component
    RequestScope
      El Componente se crea y se destruye
      en cada HTTP Request (Mas pequenio)
    
    @Component
    @SessionScope
      Es un Componente de Sesion,
      estará vivo mientras el navegador del usuario
      esté abierto y no sucesa ningun timeout (Mediano)
      El componente debe implementar la inderfaz Serializable
    
    @Component
    @ApplicationScope
       Es muy parecido Sigleton, pero la diferencia radica en que se guarda
      en el servlet context y no en el application context de spring

--- src/main/java/com/springboot/di/app/models/domain/Cliente.java
+++ src/main/java/com/springboot/di/app/models/domain/Cliente.java
@@ -2,8 +2,31 @@ package com.springboot.di.app.models.domain;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
+
+
+// Scope de los Componentes de Spring
+
+//@Componente sin nada es @Stateless de JEE, solo una instancia para la aplicacion
+//no tiene estado (Mas grande)
+//
+//@Component
+//RequestScope El Componente se crea y se destruye en cada HTTP Request (Mas pequenio)
+//
+//@Component
+//@SessionScope es un Componente de Sesion, estará vivo mientras el navegador del usuario
+//  esté abierto y no sucesa ningun timeout (Mediano)
+//  El componente debe implementar la inderfaz Serializable
+//
+//@Component
+//@ApplicationScope: Es muy parecido Sigleton, pero la diferencia radica en que se guarda
+//       en el servlet context y no en el application context de spring
+
 
 @Component
+@RequestScope
 public class Cliente {
 
   @Value("${cliente.nombre}")
--- src/main/java/com/springboot/di/app/models/domain/Factura.java
+++ src/main/java/com/springboot/di/app/models/domain/Factura.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 
 @Component
+@RequestScope
 public class Factura {
 
   @Value("${factura.descripcion}")
